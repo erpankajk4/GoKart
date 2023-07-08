e12d95797e1628c87bc315064527a1a8946f8694
erpankajk4 <118353291+erpankajk4@users.noreply.github.com>
2023-07-08 20:14:55 +0530

    productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -28,60 +28,74 @@ function generateSkuId() {
   const randomNum = Math.floor(1000 + Math.random() * 9000); // Generate a random 4-digit number
   return randomNum.toString();
 }
+/**********************************************************/
 
 // Get products by category, subcategory, or all products
 exports.getProducts = async (req, res) => {
-  try {
-    const { category, subCategory } = req.query;
-    let filter = {};
-    if (category) {
-      filter.category = category;
-    }
-    if (subCategory) {
-      filter.subCategory = subCategory;
-    }
-    const products = await Product.find(filter);
-    const productsWithStock = await Promise.all(
-      products.map(async (product) => {
-        const warehouses = await Warehouse.find({
-          'products.product': product._id,
-          'products.quantity': { $gt: 0 },
-        }).select('warehouseId');
-        const stockQuantity = warehouses.reduce(
-          (total, warehouse) =>
-            total + getWarehouseProductQuantity(warehouse, product._id),
-          0
-        );
-        product.stockQuantity = stockQuantity; // Update the stockQuantity field
-        if (warehouses.length === 0) {
+    try {
+      const { category, subCategory } = req.query;
+      let filter = {};
+      if (category) {
+        filter.category = category;
+      }
+      if (subCategory) {
+        filter.subCategory = subCategory;
+      }
+      const products = await Product.find(filter);
+      const productsWithStock = await Promise.all(
+        products.map(async (product) => {
+          const warehouses = await Warehouse.find({
+            'products.product': product._id,
+            'products.quantity': { $gt: 0 },
+          }).select('warehouseId');
+          const stockQuantity = warehouses.reduce(
+            (total, warehouse) =>
+              total + getWarehouseProductQuantity(warehouse, product._id),
+            0
+          );
+          product.stockQuantity = stockQuantity; // Update the stockQuantity field
+          if (warehouses.length === 0) {
+            return {
+              _id: product._id,
+              name: product.name,
+              sku_id: product.sku_id,
+              category: product.category,
+              subCategory: product.subCategory,
+              stockQuantity: 0,
+              inStockWarehouses: 'This product is not available in any warehouse.',
+            };
+          }
           return {
             _id: product._id,
             name: product.name,
             sku_id: product.sku_id,
             category: product.category,
             subCategory: product.subCategory,
-            stockQuantity: 0,
-            inStockWarehouses: 'This product is not available in any warehouse.',
+            stockQuantity,
+            inStockWarehouses: warehouses,
           };
-        }
-        return {
-          _id: product._id,
-          name: product.name,
-          sku_id: product.sku_id,
-          category: product.category,
-          subCategory: product.subCategory,
-          stockQuantity,
-          inStockWarehouses: warehouses,
-        };
-      })
+        })
+      );
+      res.json(productsWithStock);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to fetch products' });
+    }
+  };
+  
+// Helper function to get the product quantity in a specific warehouse
+function getWarehouseProductQuantity(warehouse, productId) {
+    if (!warehouse || !warehouse.products) {
+      return 0;
+    }
+  
+    const product = warehouse.products.find(
+      (wp) => wp.product && wp.product.toString() === productId.toString()
     );
-    res.json(productsWithStock);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to fetch products' });
+  
+    return product ? product.quantity : 0;
   }
-};
-
+/***************************************************************/
 // Delete a product by SKU ID
 exports.deleteProduct = async (req, res) => {
   try {
@@ -99,112 +113,104 @@ exports.deleteProduct = async (req, res) => {
     res.status(500).json({ error: 'Failed to delete the product' });
   }
 };
-
-// Helper function to get the product quantity in a specific warehouse
-function getWarehouseProductQuantity(warehouse, productId) {
-  if (!warehouse || !warehouse.products) {
-    return 0;
-  }
-
-  const product = warehouse.products.find(
-    (wp) => wp.product && wp.product.toString() === productId.toString()
-  );
-
-  return product ? product.quantity : 0;
-}
+/**************************************************************/
 
 // Process an order
 exports.processOrder = async (req, res) => {
-    try {
-      const { customerId, sku_id, orderQty, customerLoc } = req.body;
-  
-      // Find the customer
-      const customer = await Customer.findOne({ customerId });
-      if (!customer) {
-        return res.status(404).json({ error: 'Customer not found' });
-      }
-  
-      // Find the product
-      const product = await Product.findOne({ sku_id });
-      if (!product) {
-        return res.status(404).json({ error: 'Product not found' });
-      }
-  
-      // Check if the warehouse location is the same as the customer location
-      if (product.inStockWarehouses.some(warehouse => warehouse.location === customerLoc)) {
-        // Place the order directly
-        const order = new Order({
-          customerId: customer.customerId,
-          customerName: customer.name,
-          productName: product.name,
-          sku_id: product.sku_id,
-          fulfillmentStatus: 'Fulfilled',
-          linkedWarehouse: customerLoc,
-        });
-  
-        await order.save();
-  
-        return res.status(200).json(order);
-      }
-  
-      // Find a warehouse with the required quantity of the product
-      const warehouse = await Warehouse.findOne({
-        'products.product': product._id,
-        'products.quantity': { $gte: orderQty },
-      });
-  
-      if (!warehouse) {
-        // No warehouse has the required quantity
-        const order = new Order({
-          customerId: customer.customerId,
-          customerName: customer.name,
-          productName: product.name,
-          sku_id: product.sku_id,
-          fulfillmentStatus: 'Out of Stock',
-          linkedWarehouse: null,
-        });
-  
-        await order.save();
-  
-        return res.status(200).json(order);
-      }
-  
-      // Fulfill the order from the found warehouse
-      warehouse.products.forEach((wp) => {
-        if (wp.product.toString() === product._id.toString()) {
-          wp.quantity -= orderQty;
-        }
-      });
-  
-      await warehouse.save();
-  
+  try {
+    const { customerId, sku_id, orderQty, customerLoc } = req.body;
+
+    // Find the customer
+    const customer = await Customer.findOne({ customerId });
+    if (!customer) {
+      return res.status(404).json({ error: 'Customer not found' });
+    }
+
+    // Find the product
+    const product = await Product.findOne({ sku_id });
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Check if the warehouse location is the same as the customer location
+    const isProductAvailableInWarehouse = product.inStockWarehouses.some(
+      (warehouseId) => warehouseId.location === customerLoc
+    );
+
+    if (isProductAvailableInWarehouse) {
+      // Place the order directly
       const order = new Order({
         customerId: customer.customerId,
         customerName: customer.name,
         productName: product.name,
         sku_id: product.sku_id,
         fulfillmentStatus: 'Fulfilled',
-        linkedWarehouse: warehouse.name,
+        linkedWarehouse: customerLoc,
       });
-  
+
       await order.save();
-  
-      res.status(200).json(order);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to process the order' });
+
+      return res.status(200).json(order);
     }
-  };
-  
+
+    // Find a warehouse with the required quantity of the product
+    const warehouse = await Warehouse.findOne({
+      'products.product': product._id,
+      'products.quantity': { $gte: orderQty },
+    });
+
+    if (!warehouse) {
+      // No warehouse has the required quantity
+      const order = new Order({
+        customerId: customer.customerId,
+        customerName: customer.name,
+        productName: product.name,
+        sku_id: product.sku_id,
+        fulfillmentStatus: 'Out of Stock',
+        linkedWarehouse: null,
+      });
+
+      await order.save();
+
+      return res.status(200).json(order);
+    }
+
+    // Fulfill the order from the found warehouse
+    const productIndex = warehouse.products.findIndex(
+      (wp) => wp.product.toString() === product._id.toString()
+    );
+
+    warehouse.products[productIndex].quantity -= orderQty;
+
+    await warehouse.save();
+
+    const order = new Order({
+      customerId: customer.customerId,
+      customerName: customer.name,
+      productName: product.name,
+      sku_id: product.sku_id,
+      fulfillmentStatus: 'Fulfilled',
+      linkedWarehouse: warehouse.name,
+    });
+
+    await order.save();
+
+    res.status(200).json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to process the order' });
+  }
+};
+/******************************************************************/
 
 // View all orders
 exports.viewOrders = async (req, res) => {
-    try {
-      const orders = await Order.find();
-      res.status(200).json(orders);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Failed to fetch orders' });
-    }
-  };
-  
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch orders' });
+  }
+};
+/****************************************************************** */
