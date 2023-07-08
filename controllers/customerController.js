675d838c0d3e21326430a8bfda8f8020a586bce5
erpankajk4 <118353291+erpankajk4@users.noreply.github.com>
2023-07-08 20:12:37 +0530

    customerController.js

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -15,3 +15,22 @@ exports.addCustomer = async (req, res) => {
     res.status(500).json({ error: 'Failed to add the customer' });
   }
 };
+/******************************************************/
+// Delete customer by ID
+exports.deleteCustomerById = async (req, res) => {
+  try {
+    const { customerId } = req.params;
+    const deletedCustomer = await Customer.findOneAndDelete({ customerId });
+    if (!deletedCustomer) {
+      return res.status(404).json({ error: 'Customer not found' });
+    }
+    res.status(200).json({
+      message: 'Customer deleted successfully',
+      deletedCustomer,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to delete customer' });
+  }
+};
+/*************************************************/
